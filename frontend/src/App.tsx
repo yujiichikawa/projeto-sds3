5cf5ed2b086c7483f8f1ac74d4106a21dd282c3d
thiago yuji <gelinskiiyuji20@gmail.com>
2021-09-06 17:55:10 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
+    <>
+     <NavBar/>
     <div>
-      <h1 className="text-primary">
+      <h1 className="container">
         Olá, mundo
       </h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
