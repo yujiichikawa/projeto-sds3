fdffafad6bdf7a04ad5a44de15dbf9a49016dfcd
thiago yuji <gelinskiiyuji20@gmail.com>
2021-09-08 14:19:42 -0300

    Basic static components

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,4 @@
-function Footer() {
+function DataTable() {
     return (
         <div className="table-responsive">
     <table className="table table-striped table-sm">
@@ -130,4 +130,4 @@ function Footer() {
     );
 }
 
-export default Footer;
+export default DataTable;
